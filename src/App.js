089b87b785813eb5f58d8db82971469a8822a48b
Santiago Rozas <santiago9842@gmail.com>
2021-09-20 21:58:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Col, Row, } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid="md" className="bg-secondary justify-content-md-center">
+  <Row>
+    <Col xs lg="2">
+      NEWBRAND
+      </Col>
+    <Col xs={6} md="auto">Home</Col>
+    <Col xs lg="2">
+      Store
+      </Col>
+      <Col xs lg="2">
+      About
+      </Col>
+      <Col xs lg="2">
+      Blog
+      </Col>
+      <Col xs lg="2">
+      Point of sale
+      </Col>
+  </Row>
+  <Row>
+    <Col >
+    1 of 3
+    </Col>
+    <Col xs={5} md="auto">
+      2 of 3 (wider)
+      </Col>
+    <Col xs lg="2">
+      3 of 3
+      </Col>
+  </Row>
+</Container>
+    );
 }
 
 export default App;
