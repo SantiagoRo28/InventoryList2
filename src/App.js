3f8f6042d78dd027e85e2d4c23089d671ade61b7
santiago <santiago9842@gmail.com>
2021-09-27 21:56:55 -0300

    seven

--- src/App.js
+++ src/App.js
@@ -3,84 +3,46 @@ import './App.css';
 import { Container, Col, Row, Card, Button } from 'react-bootstrap';
 import Placeholder from 'react-bootstrap/Placeholder'
 import Nav from 'react-bootstrap/Nav'
+import { Navbar, NavDropdown } from 'react-bootstrap'
+import Carousel from 'react-bootstrap/Carousel'
 
 
 
 function App() {
   return (
     <Container fluid="md" className=" justify-content-md-center">
-  <Row>
-    <Col xs = {12} md={8} lg="2">
-      NEWBRAND
-      </Col>
-    <Col xs={6} md="auto">
-      Home
-    </Col>
-    <Col xs md={8} lg="2">
-      Store
-      </Col>
-      <Col xs md={8} lg="2">
-      About
-      </Col>
-      <Col xs md={8} lg="2">
-      Blog
-      </Col>
-      <Col xs md={8} lg="2">
-      Point of sale
-      </Col>
-      <Col xs md lg="22">
-      Cart icon
-      </Col>
-  </Row>
+      <Navbar bg="light" expand="lg">
 
-  
+        <Navbar.Brand href="#home">NEWBRAND</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Store</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Blog</Nav.Link>
+            <Nav.Link href="#link">Point of Sale</Nav.Link>
+            <Nav.Link href="#link">Cart icon</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
 
+      </Navbar>
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://www.macmillandictionary.com/us/external/slideshow/full/Grey_full.png"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
 
-   <Card>
-  <Card.Header>
-    <Nav variant="tabs" defaultActiveKey="#first">
-    <Card.Title>NEWBRAND</Card.Title>
-      <Nav.Item>
-        <Nav.Link href="#first">Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#link">Store</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#link">About</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#link">Blog</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#link">Point of sale</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#disabled" disabled>
-          Cart icon
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-  </Card.Header>
-  <Card.Body>
-    <Card.Title className="bg-secondary justify-content-md-center">New Clothing Collection</Card.Title>
-    <Button style={{ width: '18rem' }} className="bg-secondary justify-content-md-center">Shop Now</Button>
-  </Card.Body>
-</Card>
-
-
-<div className="d-flex justify-content-around">
-  <Card style={{ width: '18rem' }} xs={12} fluid="md" className="bg-secondary justify-content-md-center">
-   <Card.Body xs={12} md={8} lg="22">
-     
-   </Card.Body>
-
-
-  </Card>
-</div>
- 
-</Container>
-    );
+    </Container>
+  );
 }
 
 export default App;
