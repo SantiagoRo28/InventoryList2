fc0649be453068c083fe58338c8c1f908275efc6
santiago <santiago9842@gmail.com>
2021-11-01 21:54:00 -0300

    eight

--- src/App.js
+++ src/App.js
@@ -29,190 +29,189 @@ function App() {
       </Navbar>
       <div className="container mh-50 Carousel-container">
 
-        <Row> 
-          <div className="col-12">
-          <h1 className="Text-banner"> New Clothing Collection</h1>
-          </div>
-           
-          
-          </Row>
-        <div className="row justify-content-center"><div className="col-6 justify-content-center"><badge></badge><Button className="w-25 mr-10" variant="primary">Shop Now</Button>{' '}</div></div>
-
-
-      </div>
       
-      <div className="col-12">
-          <h1 className="Text-banner">NEW ARRIVALS</h1>
-          </div>
-          <div className="container mh-50 Mini-container">
+          <div className="d-flex flex-column justify-content-center">
+              <h1 className="Text-banner"> New Clothing Collection</h1>
+              <Button className="button-banner" variant="primary">Shop Now</Button>
 
-        <Row> 
+          </div>
+        
+
+
+      </div>
+
+      <div className="col-12">
+        <h1 className="Text-banner">NEW ARRIVALS</h1>
+      </div>
+      <div className="orisontal"> <div className="container mh-50 Mini-container">
+
+        <Row>
           <div className="col-12">
           </div>
-          </Row>
+        </Row>
       </div>
-      <h1 className="Text-product"> Product name</h1>
-      <h1 className="Text-product"> $150.00</h1>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
 
-      <div className="col-12">
+        <div className="col-12">
           <h1></h1>
-          </div>
-          <div className="container mh-50 Mini-container">
+        </div>
+        <div className="container mh-50 Mini-container">
 
-        <Row> 
-          <div className="col-12">
-          </div>
+          <Row>
+            <div className="col-12">
+            </div>
           </Row>
-      </div>
-      <h1 className="Text-product"> Product name</h1>
-      <h1 className="Text-product"> $150.00</h1>
-      <div className="col-12">
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
+        <div className="col-12">
           <h1></h1>
-          </div>
-          <div className="container mh-50 Mini-container">
+        </div>
+        <div className="container mh-50 Mini-container">
 
-        <Row> 
-          <div className="col-12">
-          </div>
+          <Row>
+            <div className="col-12">
+            </div>
           </Row>
-      </div>
-      <h1 className="Text-product"> Product name</h1>
-      <h1 className="Text-product"> $150.00</h1>
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1></div>
 
       <div className="row justify-content-center"><div className="col-6 justify-content-center"><badge></badge><Button className="w-25 mr-10" variant="primary">View More</Button>{' '}</div></div>
-      
+
       <div className="container mh-50 Carousel-container">
 
-        <Row> 
+        <Row>
           <div className="col-12">
-          <h1 className="Text-banner"> Our Story</h1>
+            <h1 className="Text-banner"> Our Story</h1>
           </div>
-           
-          
-          </Row>
+
+
+        </Row>
 
       </div>
 
       <div className="container mh-50 Carousel-container">
 
-<Row> 
-  <div className="col-12">
-  <h1 className="Text-banner"> Our Blog</h1>
-  </div>
-   
-  
-  </Row>
-<>
-  <div className="col-12">
+        <Row>
+          <div className="col-12">
+            <h1 className="Text-banner"> Our Blog</h1>
+          </div>
+
+
+        </Row>
+        <>
+          <div className="col-12">
 
           </div>
           <div className="container mh-50 Mini-container">
 
-        <Row> 
-          <div className="col-12">
-            <h1 className="Text-banner">Accessories</h1>
+            <Row>
+              <div className="col-12">
+                <h1 className="Text-banner">Accessories</h1>
+              </div>
+            </Row>
           </div>
-          </Row>
-      </div>
-
-      
-      <div className="col-12">
-
-</div>
-<div className="container mh-50 Mini-container">
-
-<Row> 
-<div className="col-12">
-  <h1 className="Text-banner">Shoes</h1>
-</div>
-</Row>
-</div>
 
 
-<div className="col-12">
+          <div className="col-12">
 
-</div>
-<div className="container mh-50 Mini-container">
+          </div>
+          <div className="container mh-50 Mini-container">
 
-<Row> 
-<div className="col-12">
-  <h1 className="Text-banner">Jackets</h1>
-</div>
-</Row>
-</div></>
+            <Row>
+              <div className="col-12">
+                <h1 className="Text-banner">Shoes</h1>
+              </div>
+            </Row>
+          </div>
 
-<div className="col-12">
+
+          <div className="col-12">
+
+          </div>
+          <div className="container mh-50 Mini-container">
+
+            <Row>
+              <div className="col-12">
+                <h1 className="Text-banner">Jackets</h1>
+              </div>
+            </Row>
+          </div></>
+
+        <div className="col-12">
           <h1 className="Text-banner">NEW ARRIVALS</h1>
-          </div>
-          <div className="container mh-50 Mini-container">
+        </div>
+        <div className="container mh-50 Mini-container">
 
-        <Row> 
-          <div className="col-12">
-          </div>
+          <Row>
+            <div className="col-12">
+            </div>
           </Row>
-      </div>
-      <h1 className="Text-product"> Product name</h1>
-      <h1 className="Text-product"> $150.00</h1>
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
 
-      <div className="col-12">
+        <div className="col-12">
           <h1></h1>
-          </div>
-          <div className="container mh-50 Mini-container">
+        </div>
+        <div className="container mh-50 Mini-container">
 
-        <Row> 
-          <div className="col-12">
-          </div>
+          <Row>
+            <div className="col-12">
+            </div>
           </Row>
-      </div>
-      <h1 className="Text-product"> Product name</h1>
-      <h1 className="Text-product"> $150.00</h1>
-      <div className="col-12">
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
+        <div className="col-12">
           <h1></h1>
-          </div>
-          <div className="container mh-50 Mini-container">
+        </div>
+        <div className="container mh-50 Mini-container">
 
-        <Row> 
-          <div className="col-12">
-          </div>
+          <Row>
+            <div className="col-12">
+            </div>
           </Row>
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
+
+        <div className="container mh-50 Mini-container">
+
+          <Row>
+            <div className="col-12">
+            </div>
+          </Row>
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
+
+        <div className="container mh-50 Mini-container">
+
+          <Row>
+            <div className="col-12">
+            </div>
+          </Row>
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
+
+        <div className="container mh-50 Mini-container">
+
+          <Row>
+            <div className="col-12">
+            </div>
+          </Row>
+        </div>
+        <h1 className="Text-product"> Product name</h1>
+        <h1 className="Text-product"> $150.00</h1>
+
+        <div className="row justify-content-center"><div className="col-6 justify-content-center"><badge></badge><Button className="w-25 mr-10" variant="primary">View More</Button>{' '}</div></div>
+
       </div>
-      <h1 className="Text-product"> Product name</h1>
-      <h1 className="Text-product"> $150.00</h1>
-
-      <div className="container mh-50 Mini-container">
-
-<Row> 
-  <div className="col-12">
-  </div>
-  </Row>
-</div>
-<h1 className="Text-product"> Product name</h1>
-<h1 className="Text-product"> $150.00</h1>
-
-<div className="container mh-50 Mini-container">
-
-<Row> 
-  <div className="col-12">
-  </div>
-  </Row>
-</div>
-<h1 className="Text-product"> Product name</h1>
-<h1 className="Text-product"> $150.00</h1>
-
-<div className="container mh-50 Mini-container">
-
-<Row> 
-  <div className="col-12">
-  </div>
-  </Row>
-</div>
-<h1 className="Text-product"> Product name</h1>
-<h1 className="Text-product"> $150.00</h1>
-
-<div className="row justify-content-center"><div className="col-6 justify-content-center"><badge></badge><Button className="w-25 mr-10" variant="primary">View More</Button>{' '}</div></div>
-
-</div>
-    </Container>
+    </Container >
   );
 }
 
